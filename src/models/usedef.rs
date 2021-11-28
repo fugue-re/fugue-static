0f7bdd9253d0d7b879ecb0ad239b473ded68c1bb
Sam Thomas <st@xv.ax>
2021-11-28 13:22:29 +0000

    dependents/dependencies using UD information

--- src/models/usedef.rs
+++ src/models/usedef.rs
@@ -1,20 +1,52 @@
-use fugue::ir::il::ecode::{Entity, EntityId, Var};
+use fugue::ir::il::ecode::{Entity, Stmt, Var};
 
 use std::collections::BTreeSet;
 
 use fxhash::FxHashMap as HashMap;
+
+use petgraph::Direction;
 use petgraph::stable_graph::StableDiGraph;
 
+use crate::graphs::entity::{EdgeIndex, VertexIndex};
 use crate::models::Block;
-use crate::graphs::entity::{VertexIndex, EdgeIndex};
 use crate::traits::*;
+use crate::types::EntityRef;
 
-pub struct UseDefs {
-    graph: StableDiGraph<Var, EntityId>,
-    variables: HashMap<Var, VertexIndex<Var>>,
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum LocalDependency<'a> {
+    Phi(EntityRef<'a, Block>, usize),
+    Stmt(EntityRef<'a, Stmt>),
 }
 
-impl Default for UseDefs {
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum Dependency<'a> {
+    Bound(LocalDependency<'a>),
+    Free,
+}
+
+impl<'a> Dependency<'a> {
+    pub fn is_free(&self) -> bool {
+        matches!(self, Self::Free)
+    }
+
+    pub fn is_bound(&self) -> bool {
+        matches!(self, Self::Bound(_))
+    }
+
+    pub fn update(&mut self, access: Dependency<'a>) {
+        if self.is_free() && !access.is_free() {
+            *self = access
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct UseDefs<'a> {
+    graph: StableDiGraph<Var, LocalDependency<'a>>,
+    variables: HashMap<Var, (VertexIndex<Var>, Dependency<'a>)>,
+}
+
+impl<'a> Default for UseDefs<'a> {
     fn default() -> Self {
         Self {
             graph: StableDiGraph::new(),
@@ -23,7 +55,7 @@ impl Default for UseDefs {
     }
 }
 
-impl UseDefs {
+impl<'a> UseDefs<'a> {
     pub fn new() -> Self {
         Self::default()
     }
@@ -32,30 +64,36 @@ impl UseDefs {
         self.graph.node_count()
     }
 
-    pub fn add_var(&mut self, var: Var) -> VertexIndex<Var> {
-        if let Some(vx) = self.variables.get(&var) {
+    pub fn add_var(&mut self, var: Var, access: Dependency<'a>) -> VertexIndex<Var> {
+        if let Some((vx, caccess)) = self.variables.get_mut(&var) {
+            caccess.update(access);
             *vx
         } else {
             let vx = self.graph.add_node(var).into();
-            self.variables.insert(var, vx);
+            self.variables.insert(var, (vx, access));
             vx
         }
     }
 
-    pub fn add_use_def(&mut self, u: Var, d: Var, e: EntityId) -> EdgeIndex<EntityId> {
-        let ux = self.add_var(u);
-        let dx = self.add_var(d);
+    pub fn add_use_def(
+        &mut self,
+        u: Var,
+        d: Var,
+        dep: LocalDependency<'a>,
+    ) -> EdgeIndex<LocalDependency<'a>> {
+        let ux = self.add_var(u, Dependency::Free);
+        let dx = self.add_var(d, Dependency::Bound(dep.clone()));
 
-        let du = self.graph.add_edge(*dx, *ux, e);
+        let du = self.graph.add_edge(*ux, *dx, dep);
 
         du.into()
     }
 
-    pub fn add_block(&mut self, block: Entity<Block>) {
-        let pid = EntityId::new("phi", block.location().clone());
-        for (d, us) in block.phis().iter() {
+    pub fn add_block(&mut self, block: &'a Entity<Block>) {
+        for (i, (d, us)) in block.phis().iter().enumerate() {
             for u in us {
-                self.add_use_def(*u, *d, pid.clone());
+                let r = block.into_entity_ref();
+                self.add_use_def(*u, *d, LocalDependency::Phi(r, i));
             }
         }
 
@@ -63,18 +101,56 @@ impl UseDefs {
         let mut us = BTreeSet::new();
 
         for op in block.operations() {
-            let eid = op.id().clone();
-
             op.defined_and_used_variables_with(&mut ds, &mut us);
 
             for d in ds.iter() {
                 for u in us.iter() {
-                    self.add_use_def(**u, **d, eid.clone());
+                    let r = op.into_entity_ref();
+                    self.add_use_def(**u, **d, LocalDependency::Stmt(r));
                 }
             }
 
             ds.clear();
             us.clear();
         }
+    }
+
+    pub fn add_stmt(&mut self, stmt: &'a Entity<Stmt>) {
+        let (ds, us) = stmt.defined_and_used_variables::<BTreeSet<_>>();
+
+        for d in ds.iter() {
+            for u in us.iter() {
+                let r = stmt.into_entity_ref();
+                self.add_use_def(**u, **d, LocalDependency::Stmt(r));
+            }
+        }
+    }
+
+    pub fn dependents(&self, var: &Var) -> Vec<(&Var, &LocalDependency<'a>)> {
+        let mut deps = Vec::new();
+        if let Some((vx, _)) = self.variables.get(var) {
+            let mut walker = self.graph.neighbors_directed(**vx, Direction::Outgoing).detach();
+
+            while let Some((e, n)) = walker.next(&self.graph) {
+                let v = &self.graph[n];
+                let d = &self.graph[e];
+
+                deps.push((v, d));
+            }
+        }
+        deps
+    }
+
+    pub fn dependencies(&self, var: &Var) -> Vec<(&Var, &Dependency<'a>)> {
+        let mut deps = Vec::new();
+        if let Some((vx, _)) = self.variables.get(var) {
+            for n in self.graph.neighbors_directed(**vx, Direction::Incoming) {
+                let v = &self.graph[n];
+                let d = &self.variables[v].1;
+
+                deps.push((v, d));
+            }
+        }
+        deps
     }
 }
